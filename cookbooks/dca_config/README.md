fc5e72a494a40f0fba8616432d226c899e9951e9
karen <kkwentus@chef.io>
2018-03-12 20:24:43 -0500

    cookbook updates

--- cookbooks/dca_config/README.md
+++
@@ -1,5 +0,0 @@
-# dca_config
-
-TODO: Enter the cookbook description here.
-
-The cookbook that configures nodes for the DCA workshop
--- cookbooks/dca_config/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook:: dca_config
-# Recipe:: default
-#
-# Copyright:: 2018, The Authors, All Rights Reserved.
-
-
-include_recipe 'chef-client::default'
-include_recipe 'audit::default'
